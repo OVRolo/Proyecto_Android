f2781373046ac2c2bdd392e911631c5bb0c44412
JCMUNOZ\Juan Carlos Muñoz <jcml2704@gmail.com>
2018-02-08 10:46:41 +0100

    datos

--- app/src/main/java/com/example/juancarlosmuoz/myapplication/pagina2.java
+++ app/src/main/java/com/example/juancarlosmuoz/myapplication/pagina2.java
@@ -4,20 +4,30 @@ import android.drm.DrmStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class pagina2 extends AppCompatActivity {
 
-    private TextView tv_datos;
+
     Usuario usu;
+    private EditText etNombre;
+    private EditText etApellido;
+    private EditText etTelefono;
+    private EditText etEmail;
+    private EditText etPuesto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pagina2_);
 
-        tv_datos=(TextView)findViewById(R.id.tv_datos);
-
+        etNombre=(EditText)findViewById(R.id.etNombre);
+        etApellido=(EditText)findViewById(R.id.etApellido);
+        etTelefono=(EditText)findViewById(R.id.etTelefono);
+        etEmail=(EditText)findViewById(R.id.etEmail);
+        etPuesto=(EditText)findViewById(R.id.etPuesto);
 
         usu =(Usuario) getIntent().getSerializableExtra("usu");
     }
@@ -25,8 +35,11 @@ public class pagina2 extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        tv_datos.setText(usu.getNombre());
-
+        etNombre.setText(usu.getNombre());
+        etApellido.setText(usu.getApellido());
+        etTelefono.setText(usu.getTelefono());
+        etEmail.setText(usu.getEmail());
+        etPuesto.setText(usu.getPuesto());
 
     }
     public void salir(View v) {
